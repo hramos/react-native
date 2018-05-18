c5679ce5a9f34d91c2f8b12d57489dac54a6f917
Hector Ramos <hramos@fb.com>
2018-05-18 14:36:31 -0700

    Ensure sdk packages are installed on CI

--- scripts/android-setup.sh
+++ scripts/android-setup.sh
@@ -6,26 +6,24 @@ function getAndroidPackages {
   export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools.bin:$PATH"
 
   # Package names can be obtained using `sdkmanager --list`
-  if [ ! $CI ]; then
-    echo "Installing Android API level $ANDROID_SDK_TARGET_API_LEVEL, Google APIs, $AVD_ABI system image..."
-    echo "y" | sdkmanager "system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;$AVD_ABI"
-    echo "Installing build SDK for Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
-    echo "y" | sdkmanager "platforms;android-$ANDROID_SDK_BUILD_API_LEVEL"
-    echo "Installing target SDK for Android API level $ANDROID_SDK_TARGET_API_LEVEL..."
-    echo "y" | sdkmanager "platforms;android-$ANDROID_SDK_TARGET_API_LEVEL"
-    echo "Installing SDK build tools, revision $ANDROID_SDK_BUILD_TOOLS_REVISION..."
-    echo "y" | sdkmanager "build-tools;$ANDROID_SDK_BUILD_TOOLS_REVISION"
-    # These moved to "system-images;android-$ANDROID_SDK_BUILD_API_LEVEL;google_apis;x86" starting with API level 25, but there is no ARM version.
-    if [ $ANDROID_GOOGLE_API_LEVEL -lt 25]; then
-      echo "Installing Google APIs $ANDROID_GOOGLE_API_LEVEL..."
-      echo "y" | sdkmanager "add-ons;addon-google_apis-google-$ANDROID_GOOGLE_API_LEVEL"
-    else
-      echo "Installing Google APIs $ANDROID_GOOGLE_API_LEVEL x86..."
-      echo "y" | sdkmanager "system-images;android-$ANDROID_GOOGLE_API_LEVEL;google_apis;x86"
-    fi
-    echo "Installing Android Support Repository"
-    echo "y" | sdkmanager "extras;android;m2repository"
+  echo "Installing Android API level $ANDROID_SDK_TARGET_API_LEVEL, Google APIs, $AVD_ABI system image..."
+  echo "y" | sdkmanager "system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;$AVD_ABI"
+  echo "Installing build SDK for Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
+  echo "y" | sdkmanager "platforms;android-$ANDROID_SDK_BUILD_API_LEVEL"
+  echo "Installing target SDK for Android API level $ANDROID_SDK_TARGET_API_LEVEL..."
+  echo "y" | sdkmanager "platforms;android-$ANDROID_SDK_TARGET_API_LEVEL"
+  echo "Installing SDK build tools, revision $ANDROID_SDK_BUILD_TOOLS_REVISION..."
+  echo "y" | sdkmanager "build-tools;$ANDROID_SDK_BUILD_TOOLS_REVISION"
+  # These moved to "system-images;android-$ANDROID_SDK_BUILD_API_LEVEL;google_apis;x86" starting with API level 25, but there is no ARM version.
+  if [ $ANDROID_GOOGLE_API_LEVEL -lt 25]; then
+    echo "Installing Google APIs $ANDROID_GOOGLE_API_LEVEL..."
+    echo "y" | sdkmanager "add-ons;addon-google_apis-google-$ANDROID_GOOGLE_API_LEVEL"
+  # else
+  #   echo "Installing Google APIs $ANDROID_GOOGLE_API_LEVEL x86..."
+  #   echo "y" | sdkmanager "system-images;android-$ANDROID_GOOGLE_API_LEVEL;google_apis;x86"
   fi
+  echo "Installing Android Support Repository"
+  echo "y" | sdkmanager "extras;android;m2repository"
 }
 
 function getAndroidNDK {
